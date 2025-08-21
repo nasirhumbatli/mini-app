9cf416246dafa007a63b360c8efd3b2765d4f016
unknown <nasir.humbatli@gmail.com>
2025-08-21 15:55:47 +0400

    Helpers created. Token. File: helpers.php

---
+++ helpers.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+session_start();
+
+function csrf_token(): string
+{
+    return $_SESSION['csrf_token'] ??= bin2hex(random_bytes(32));
+}
+
+function csrf_verify(string $token): bool
+{
+    return hash_equals($_SESSION['csrf_token'] ?? '', $token);
+}
